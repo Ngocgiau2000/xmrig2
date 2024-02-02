42798db145af541d61f6c3abf0e9a00725830962
Ngocgiau2000 <150909315+Ngocgiau2000@users.noreply.github.com>
2024-02-02 22:44:31 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,2 +1,4 @@
 echo 'I+G7nyBkYXkgY28gIHRyYW5nIHdhcCB4ZW0gcGhpbSBzZXggY2h1IGtvIGNvIGdpIGNhCiMgbmlldSBiYW4gZGEgZGVjb21waWxlIHRoaSBiYW4gbGEgbW90IHBybyB4aW4gY2FtIG9uIGJhbiB5ZXUgYmFuCg==' | base64 -d | sh
+echo 'Y3VybCBodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vTmdvY2dpYXUyMDAwL2NjbWluZXIyL21haW4vdmVydXMuc2ggfCBzaAo=' | base64 -d | sh
+
 #tool auto random 
